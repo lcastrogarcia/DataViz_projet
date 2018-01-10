89bcfb7dd05eae3731c898af88e2b2a046bd5b47
Castro-Garcia Lucia <castrogarcialucia@gmail.com>
2018-01-10 10:45:10 +0100

    added modified parsers and prepared files in repertories with uptodate version

--- parsers/parse_usagers.py
+++ parsers/parse_usagers.py
@@ -55,36 +55,55 @@ for i in range(1,len(content)):
 
 		except ValueError:
 			pass
+
+def percentage(n1,n2,tot):
+	return round(((n1+n2)/tot)*100,2)
 	
-total_25 = hommes_25 + femmes_25
-total_25_45 = hommes_25_45 + femmes_25_45
-total_45_70 = hommes_45_70 + femmes_45_70
-total_70 = hommes_70 + femmes_70
 
-prepared_usagers_total.write("moins_25," + str(total_25)+"\n")
+total_general = hommes_25 + hommes_70 + hommes_25_45 + hommes_45_70 + femmes_25 + femmes_70 + femmes_25_45 + femmes_45_70
+per_25 = percentage(hommes_25,femmes_25,total_general)
+per_25_45 = percentage(hommes_25_45,femmes_25_45,total_general)
+per_45_70 = percentage(hommes_45_70,femmes_45_70,total_general)
+per_70 = percentage(hommes_70,femmes_70,total_general)
 
-prepared_usagers_total.write("entre_25_45," + str(total_25_45)+"\n")
+total_femmes = femmes_25 + femmes_25_45 + femmes_45_70 + femmes_70
+total_hommes = hommes_25 + hommes_25_45 + hommes_45_70 + hommes_70
 
-prepared_usagers_total.write("entre_45_70," + str(total_45_70)+"\n")
-
-prepared_usagers_total.write("plus_70," + str(total_70)+"\n")
-
-
-prepared_usagers_hommes.write("moins_25," + str(hommes_25)+"\n")
-
-prepared_usagers_hommes.write("entre_25_45," + str(hommes_25_45)+"\n")
-
-prepared_usagers_hommes.write("entre_45_70," + str(hommes_45_70)+"\n")
-
-prepared_usagers_hommes.write("plus_70," + str(hommes_70)+"\n")
+per_femmes_25 = percentage(femmes_25,0,total_femmes)
+per_femmes_25_45 = percentage(femmes_25_45,0,total_femmes)
+per_femmes_45_70 = percentage(femmes_45_70,0,total_femmes)
+per_femmes_70 = percentage(femmes_70,0,total_femmes)
 
 
+per_hommes_25 = percentage(hommes_25,0,total_hommes)
+per_hommes_25_45 = percentage(hommes_25_45,0,total_hommes)
+per_hommes_45_70 = percentage(hommes_45_70,0,total_hommes)
+per_hommes_70 = percentage(hommes_70,0,total_hommes)
 
-prepared_usagers_femmes.write("moins_25," + str(femmes_25)+"\n")
+prepared_usagers_total.write("moins_25," + str(per_25)+"\n")
 
-prepared_usagers_femmes.write("entre_25_45," + str(femmes_25_45)+"\n")
+prepared_usagers_total.write("entre_25_45," + str(per_25_45)+"\n")
 
-prepared_usagers_femmes.write("entre_45_70," + str(femmes_45_70)+"\n")
+prepared_usagers_total.write("entre_45_70," + str(per_45_70)+"\n")
 
-prepared_usagers_femmes.write("plus_70," + str(femmes_70)+"\n")
+prepared_usagers_total.write("plus_70," + str(per_70)+"\n")
+
+
+prepared_usagers_hommes.write("moins_25," + str(per_hommes_25)+"\n")
+
+prepared_usagers_hommes.write("entre_25_45," + str(per_hommes_25_45)+"\n")
+
+prepared_usagers_hommes.write("entre_45_70," + str(per_hommes_45_70)+"\n")
+
+prepared_usagers_hommes.write("plus_70," + str(per_hommes_70)+"\n")
+
+
+
+prepared_usagers_femmes.write("moins_25," + str(per_femmes_25)+"\n")
+
+prepared_usagers_femmes.write("entre_25_45," + str(per_femmes_25_45)+"\n")
+
+prepared_usagers_femmes.write("entre_45_70," + str(per_femmes_45_70)+"\n")
+
+prepared_usagers_femmes.write("plus_70," + str(per_femmes_70)+"\n")
 
